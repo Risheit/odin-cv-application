2fc0c4bd6bd193353d0ca062324b5dde3edd5ace
Risheit <80658104+Risheit@users.noreply.github.com>
2022-08-30 01:24:07 -0500

    Create InfoCard

--- src/components/InfoCard.tsx
+++ src/components/InfoCard.tsx
@@ -1,8 +1,28 @@
-import '../styles/Components.css'
+import "../styles/Components.css";
+import uniqid from "uniqid";
 
-export default function InfoCard(props: any) {
-    return (
-        <section className=''>
-        </section>
-    );
+type Props = {
+  header: string;
+  period: string;
+  subtitle?: string;
+  listName: string;
+  list: string[];
 };
+
+export default function InfoCard(props: Props) {
+  return (
+    <section className="Card">
+      <h1 className="Title">
+        {props.header}
+        <span className="Period">{props.period}</span>
+      </h1>
+      {props.subtitle ? <h2>{props.subtitle}</h2> : null}
+      <h3 className="List">{props.listName}</h3>
+      <ul>
+        {props.list.map((value) => (
+          <li key={uniqid()}>{value}</li>
+        ))}
+      </ul>
+    </section>
+  );
+}
