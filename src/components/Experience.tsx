c32867b23a3c43460b7e4607b365131551def658
Risheit <80658104+Risheit@users.noreply.github.com>
2022-08-30 01:41:42 -0500

    Add Cards to Education section

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,9 +1,29 @@
-import '../styles/Components.css';
+import { useState } from "react";
+import "../styles/Components.css";
+import InfoCard from "./InfoCard";
 
 export default function Experience() {
+  const [cards, setCards] = useState([]);
+
+  const addCard = (
+    institution: string,
+    period: string,
+    degree: string,
+    awards: string[]
+  ) => (
+    <InfoCard
+      header={institution}
+      period={period}
+      subtitle={degree}
+      listName="Awards"
+      list={awards}
+    />
+  );
+
   return (
-    <section className='Component' id='Experience'>
+    <section className="Component" id="Experience">
       <h1>Experience</h1>
+      <div className="Cards">{cards}</div>
     </section>
   );
 }
