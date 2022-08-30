d1116fd506ffda55f6bbe25dd76eb01f7296c992
Risheit <80658104+Risheit@users.noreply.github.com>
2022-08-30 00:40:15 -0500

    Make Info edits happen in form

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,40 +1,63 @@
-import { useState } from "react";
+import React, { FormEvent, SyntheticEvent, useState } from "react";
 import "../styles/Components.css";
 // import parsePhoneNumber, { PhoneNumber } from "libphonenumber-js";
 
-type FormProps = {
-  name: string,
-  setName: React.Dispatch<React.SetStateAction<string>>;
-  phone: string,
-  setPhone: React.Dispatch<React.SetStateAction<string>>;
-  address: string,
-  setAddress: React.Dispatch<React.SetStateAction<string>>;
-  email: string,
-  setEmail: React.Dispatch<React.SetStateAction<string>>;
-};
-
-function EditInfoForm(props: FormProps) {
-  
-}
+import CVForm from "./CVForm";
 
 export default function Info() {
   const [name, setName] = useState("");
   const [phone, setPhone] = useState("");
   const [address, setAddress] = useState("");
   const [email, setEmail] = useState("");
+  const [isEditable, setIsEditable] = useState(false);
+
+  const handleClick = (e: SyntheticEvent) => setIsEditable(!isEditable);
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    setIsEditable(false);
+  };
 
   return (
     <section className="Component" id="Info">
       <div className="header">
         <h1>Info</h1>
-        <button className="editInfo">Edit</button>
+        <button className="editInfo" onClick={handleClick}>
+          Edit
+        </button>
       </div>
-      <div>
-        <p><strong>Name</strong>: {name}</p>
-        <p><strong>Phone</strong>: {phone}</p>
-      </div>
-      <p><strong>Address</strong>: {address}</p>
-      <p><strong>Email</strong>: {email}</p>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <p>
+            <label htmlFor="name">Name</label>
+            <input type="text" name="name" id="name" disabled={!isEditable} />
+          </p>
+          <p>
+            <label htmlFor="phone">Phone</label>
+            <input type="number" name="phone" id="phone" disabled={!isEditable} />
+          </p>
+        </div>
+        <div>
+          <p>
+            <label htmlFor="address">Address</label>
+            <input
+              type="text"
+              name="address"
+              id="address"
+              disabled={!isEditable}
+            />
+          </p>
+          <p>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              disabled={!isEditable}
+            />
+          </p>
+        </div>
+      </form>
     </section>
   );
 }
